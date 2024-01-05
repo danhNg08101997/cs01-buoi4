1b7a8d45c7146d01e1431fa6ca6ba22e171aec42
danhNg08101997 <danh.nguyenthanh102@gmail.com>
2024-01-05 11:00:56 +0700

    giai_thua update

--- giai_thua.py
+++ giai_thua.py
@@ -3,11 +3,11 @@
 
 # Input
 so_n = int(input("Nhập vào số n: "))
+while so_n < 0:
+    so_n = int(input("Nhập vào số n: "))
 # Output
 gia_thua = 1
 # Process
-# if so_n < 1:
-#     print("Gia thừa là: ", gia_thua)
 so_hang = 1
 while so_hang <= so_n:
     gia_thua *= so_hang
